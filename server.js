33de29cfddbd9bb7454a139ae9e11d8741b90529
Amartin02 <Tankfurnace641@gmail.com>
2024-04-05 10:03:00 -0600

    changed routes to note use public directory since it doesnt exist anymore

--- server.js
+++ server.js
@@ -12,13 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // GET Route for homepage
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 // GET Route for feedback page
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "/notes.html"))
 );
 
 app.get("/api/notes", (req, res) => {
